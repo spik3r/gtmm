a2c882f531b89c3f4710a439c7b0445c70fa8512
Kai Tait <kai.tait@guvera.com>
2016-06-25 22:21:44 +1000

    updating mail form

--- mailform.php
+++ mailform.php
@@ -1,34 +1,15 @@
 <?php
-$action=$_REQUEST['action'];
-if ($action=="")    /* display the contact form */
-    {
-    ?>
-    <form  action="" method="POST" enctype="multipart/form-data">
-    <input type="hidden" name="action" value="submit">
-    Your name:<br>
-    <input name="name" type="text" value="" size="30"/><br>
-    Your email:<br>
-    <input name="email" type="text" value="" size="30"/><br>
-    Your message:<br>
-    <textarea name="message" rows="7" cols="30"></textarea><br>
-    <input type="submit" value="Send email"/>
-    </form>
-    <?php
-    }
-else                /* send the submitted data */
-    {
-    $name=$_REQUEST['name'];
-    $email=$_REQUEST['email'];
-    $message=$_REQUEST['message'];
-    if (($name=="")||($email=="")||($message==""))
-        {
-		echo "All fields are required, please fill <a href=\"\">the form</a> again.";
-	    }
-    else{
+    $name=$_POST['name'];
+    $email=$_POST['email'];
+    $message=$_POST['message'];
+
+    if (($name=="")||($email=="")||($message=="")){
+		echo "All fields are required";
+
+	}else{
 	    $from="From: $name<$email>\r\nReturn-path: $email";
         $subject="Message sent using your contact form";
 		mail("kaiftait@gmail.com", $subject, $message, $from);
 		echo "Email sent!";
-	    }
     }
 ?>
