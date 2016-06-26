bfb75fd448616e968907465d6d2f2150db532866
Kai Tait <kai.tait@guvera.com>
2016-06-26 15:15:17 +1000

    trying to fix mailer

--- mailform.php
+++ mailform.php
@@ -4,7 +4,7 @@
     $message=$_POST['message'];
     $headers  = 'MIME-Version: 1.0' . "\r\n";
     $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-    $headers .= 'Cc: birthdayarchive@example.com' . "\r\n";
+    $headers .= 'Cc: kaiftait@gmail.com' . "\r\n";
     if (($name=="")||($email=="")||($message=="")){
 		echo "All fields are required";
 
