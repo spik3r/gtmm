f5a7369ee0b12cb270926201c83d60caec63eb90
Kai Tait <kai.tait@guvera.com>
2016-06-26 15:14:46 +1000

    trying to fix mailer

--- mailform.php
+++ mailform.php
@@ -2,14 +2,16 @@
     $name=$_POST['name'];
     $email=$_POST['email'];
     $message=$_POST['message'];
-
+    $headers  = 'MIME-Version: 1.0' . "\r\n";
+    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+    $headers .= 'Cc: birthdayarchive@example.com' . "\r\n";
     if (($name=="")||($email=="")||($message=="")){
 		echo "All fields are required";
 
 	}else{
 	    $from="From: $name<$email>\r\nReturn-path: $email";
         $subject="Contact form enquiry";
-		mail("gt.mm@hotmail.com", $subject, $message, $from);
+		mail("gt.mm@hotmail.com", $subject, $message, $from, $headers);
 		echo "Email sent!";
     }
 ?>
