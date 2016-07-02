e37ee1a2afe0e0126cdb25d7547ea9875353547a
Kai Tait <kai.tait@guvera.com>
2016-07-02 10:54:13 +1000

    updated meta

---
+++ js/context.js
@@ -0,0 +1,11 @@
+type="application/ld+json">
+    {
+        "@context" : "http://schema.org",
+        "@type" : "Organization",
+        "name" : "GT Maintenance & Mechanical",
+        "url" : "http://www.gtmm.com.au",
+        "sameAs" : [
+            "http://gtmm.com.au",
+            "https://www.facebook.com/gtmm.com.au/"
+        ]
+    }
